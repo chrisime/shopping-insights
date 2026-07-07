c6bb0516000c59d09c8026051fd8cac82abb7d07
Christian Meyer <github.ywa1n@passmail.net>
2026-07-07 19:16:30 +0200

    fix: backend start due to missing requirement

--- start_backend.sh
+++ start_backend.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -eu
 
-exec "$(dirname "$0")/.venv/bin/python" -m uvicorn api.main:app --reload --port 8000
+exec "$(dirname "$0")/venv/bin/python" -m uvicorn api.main:app --reload --port 8000
